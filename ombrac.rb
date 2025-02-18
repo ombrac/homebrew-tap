fdbecb9c598a81d01a31a167901ef21a64fde272
ioaths <github@ioaths.com>
2025-02-18 14:14:24 +0800

    Update ombrac.rb

--- ombrac.rb
+++ ombrac.rb
@@ -17,10 +17,8 @@ class Ombrac < Formula
   # end
 
   def install
-    # Remove unrecognized options if they cause configure to fail
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", "--disable-silent-rules", *std_configure_args
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "ombrac-client"
+    bin.install "ombrac-server"
   end
 
   test do
